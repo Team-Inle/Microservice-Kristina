becfc168d41eaa49368c5f8831fd89eb7c9f046e
Kristina <59376760+kristinarmontanez@users.noreply.github.com>
2021-12-03 22:56:56 -0800

    Going over some code smells.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 #||||||||||||||||||||||||||||||||||||||| INDEX.JS |||||||||||||||||||||||||||||||||||||||||
 
 # TO RUN THE APP LOCALLY, USE THE ENVIRONMENT SETUP:
+#  . venv/bin/activate
 
-#           . venv/bin/activate
 
-#  When you’re done, to deactivate the flask environment: 
 
-#           deactivate
+
+
+
+
 
 #__________________________________ IMPORT SETUP ___________________________________________
 
@@ -31,28 +33,23 @@ api = Api(app)
 
 class songWiki(Resource):
     def get(self, artist):
-        # URL
         URL = "https://en.wikipedia.org/wiki/" 
-        # send the request to Wikipedia song page.
         response = requests.get(URL + artist)
-        # parse the response
         soup = bs4.BeautifulSoup(response.text, 'html.parser') 
+        
+        
+        
         # we have to find artist title.
         title = soup.find('h1', {'class': 'firstHeading'})
-        # getting the intro paragraph.
         paragraphs = soup.select("p")
-        # just grab the text up to contents as stated in question
         intro_paragraph = '\n'.join([ para.text for para in paragraphs[0:2]])
         intro_paragraph = re.sub(r'\[[0-9]*\]',' ', intro_paragraph)
-        #intro_paragraph = re.sub(r'\s+',' ', intro_paragraph)
-        #intro_paragraph = intro_paragraph.strip('\\\\') # does not work yet.
-        #intro_paragraph = re.sub(r'\n','', intro_paragraph)
-        # get it all together
         tune_scout = {
                         "Artist": title.text,
                         "About": intro_paragraph
                     }
 
+        
         # printing the whole thing.
         return jsonify(tune_scout)
 
